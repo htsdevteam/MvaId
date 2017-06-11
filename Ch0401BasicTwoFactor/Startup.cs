5e802a9a7c64843bd672739de5c0d6284ed59a25
serge <serge@serge.com>
2017-06-11 18:20:38 +0300

    Ch4 - Two factor authentication

---
+++ Ch0401BasicTwoFactor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ch0401BasicTwoFactor.Startup))]
+namespace Ch0401BasicTwoFactor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
